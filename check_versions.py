5ed5f504810440e110fda88aa5487a734aa13bca
zhenliu <liuzhen2010@gmail.com>
2017-11-03 21:54:37 -0700

    Update check_versions.py

--- check_versions.py
+++ check_versions.py
@@ -1,1 +1,26 @@
 import sys
+print("Python version: {}".format(sys.version))
+
+import pandas as pd
+print("pandas version: {}".format(pd.__version__))
+
+import matplotlib
+print("matplotlib version: {}".format(matplotlib.__version__))
+
+import numpy as np
+print("Numpy version: {}".format(np.__version__))
+
+import scipy as sp
+print("Scipy version: {}".format(sp.__version__))
+
+import IPython
+print("IPython version: {}".format(IPython.__version__))
+
+import sklearn
+print("scikit-learn version: {}".format(sklearn.__version__))
+
+import tensorflow as tf
+print("tensorflow version: {}".format(tf.__version__))
+
+import keras
+print("keras version: {}".format(keras.__version__))
